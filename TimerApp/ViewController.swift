0610e26117ccf526a7f28d54565110d7393a11e0
vagrant <vagrant@localhost.localdomain>
2021-01-17 08:35:10 +0000

    Button設置

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //stopButtonを押せなくする
+        stopButton.isEnabled = true
+        
+        //プロパティー
     }
-
-
+    
+    @IBAction func start(_ sender: Any) {
+        //imageViewのimageに画像を反映していく
+        
+        //startButtonは押せなくする
+        startButton.isEnabled = true
+        
+        
+    }
+    
+    
+    @IBAction func stop(_ sender: Any) {
+        
+        //imageViewのimageに反映されている画像の流れをStopする
+        
+        //startButtonを押せるようにする
+        startButton.isEnabled = false
+        
+    }
+    
+    
 }
 
