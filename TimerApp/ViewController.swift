dd2d304f5a0e247e254e0957a0530b7360223493
vagrant <vagrant@localhost.localdomain>
2021-01-17 10:09:27 +0000

    imageルーレット機能作成

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -15,21 +15,64 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var stopButton: UIButton!
     
+    var timer = Timer()
+    
+    var count = Int()
+    
+    var imageArray = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //stopButtonを押せなくする
-        stopButton.isEnabled = true
+        count = 0
         
-        //プロパティー
+        //stopButtonを押せなくする//プロパティー
+        stopButton.isEnabled = false
+         
+        for i in 0..<5{
+            
+            print(i)
+            
+            let image = UIImage(named: "\(i)")
+            imageArray.append(image!)
+            
+        }
+        
+        imageView.image = UIImage(named: "0")
+        
+    }
+    
+    func startTimer(){
+        
+        //タイマーを回す　0.2秒毎にあるメソッドを呼ぶ
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+    }
+    //0.2秒毎に呼ばれる
+    @objc func timerUpdate(){
+        
+        count = count + 1
+        
+        if count > 4 {
+            
+            count = 0
+        }
+        
+        imageView.image = imageArray[count]
+        
+        
+        //imageArray[]
+        
     }
     
     @IBAction func start(_ sender: Any) {
         //imageViewのimageに画像を反映していく
         
+        startTimer()
+        
         //startButtonは押せなくする
-        startButton.isEnabled = true
+        startButton.isEnabled = false
+        stopButton.isEnabled = true
         
         
     }
@@ -40,7 +83,11 @@ class ViewController: UIViewController {
         //imageViewのimageに反映されている画像の流れをStopする
         
         //startButtonを押せるようにする
-        startButton.isEnabled = false
+        startButton.isEnabled = true
+        stopButton.isEnabled = false
+        //タイマーを止める
+        timer.invalidate()
+        
         
     }
     
